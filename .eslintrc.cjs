d85a088a5d2b5cd6abf14b32df635aca44633045
Daniel Dutka <danieldtk@gmail.com>
2024-03-07 11:22:29 +0100

    add vercel eslint prettier tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,16 +4,11 @@ const project = resolve(__dirname, 'tsconfig.json');
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
   extends: [
-    require.resolve('@vercel/style-guide/eslint/browser'),
     require.resolve('@vercel/style-guide/eslint/typescript'),
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
     project,
-    tsconfigRootDir: __dirname,
   },
   settings: {
     'import/resolver': {
